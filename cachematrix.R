5068f003ec27fa1b2c4fe43500abd1d6460b18a2
Vikeshk <vikesh_kumar1@yahoo.co.in>
2016-03-29 10:23:07 -0500

    Cache matrix function version 1.1

--- cachematrix.R
+++ cachematrix.R
@@ -1,21 +1,34 @@
-## makeCacheMatrix function caches the value in global variable y
-## Then y can be used in the environment other then its local evironment
+## makeCacheMatrix 
+## function caches the inverse of a matrix in global variable y
+## 1) Initialize y,z & inv variable as NULL
+## 2) Assign input matrix to global variable z
+## 3) Assign inverse of input matrix to local variable inv
+## 4) Assign value of inv to global variable y
+## 5) Store original matrix (x variable) and inverse of matrix (inv variable) 
+##    in a list       
 
 
 makeCacheMatrix <- function(x = matrix()) {
   y <- NULL
   z <- NULL
+  inv <- NULL
 
   z <<- x
-  y <<- solve(x)
-
+  inv <- solve(x)
+  y <<- inv
+list(original = x,
+    inverse = inv)
 
 }
 
-
+## cachesolve
 ## This function checks whether the inverse of matrix is already available. 
-## If available then returns the cached value else calculates the inverse of new matrix
-
+## If available then it returns the cached value else calculates the inverse of new matrix
+## 1) Initialize variable t as NULL
+## 2) Check whether inverse of a matrix is already available and has some value
+## 3) If inverse found then return from cache
+## 4) Else re calculate the inverse using function makecackematrix
+## 5) Return the inverse of a matrix
 cacheSolve <- function(x, ...) {
   t <- NULL
  
@@ -26,6 +39,6 @@ cacheSolve <- function(x, ...) {
   
   
   t <- makeCacheMatrix(x)  
-  return(y)
+  return(t$inverse)
   
 }
